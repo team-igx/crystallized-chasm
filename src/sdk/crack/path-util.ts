3af53ded744486d0e5ff806b6ae7282f82610735
skywolf46 <skywolf461@gmail.com>
2026-09-19 23:23:31 +0900

    feat(C2/CRCK): 크랙 path api에 URL 파라미터 추가

--- src/sdk/crack/path-util.ts
+++ src/sdk/crack/path-util.ts
@@ -10,74 +10,81 @@ export enum CrackerTabType {
 }
 /**
  * 현재 URL이 크랙 대시보드 URL인지 반환합니다.
+ * @param [url=location] 대상 URL. 지정되지 않았을 경우, 현재 웹 페이지의 URL을 기준으로 합니다.
  * @returns 대시보드 여부
  */
-function isDashboardPath(): boolean {
-  return "/" === location.pathname;
+function isDashboardPath(url: URL | Location = location): boolean {
+  return "/" === url.pathname;
 }
 
 /**
  * 현재 URL이 스토리챗의 URL인지 반환합니다.
+ * @param [url=location] 대상 URL. 지정되지 않았을 경우, 현재 웹 페이지의 URL을 기준으로 합니다.
  * @returns 채팅 URL 일치 여부
  */
-function isStoryPath(): boolean {
+function isStoryPath(url: URL | Location = location): boolean {
   // 2025-09-17 Path
   return (
-    /\/stories\/[a-f0-9]+\/episodes\/[a-f0-9]+/.test(location.pathname) ||
+    /\/stories\/[a-f0-9]+\/episodes\/[a-f0-9]+/.test(url.pathname) ||
     // Legacy Path
-    /\/u\/[a-f0-9]+\/c\/[a-f0-9]+/.test(location.pathname)
+    /\/u\/[a-f0-9]+\/c\/[a-f0-9]+/.test(url.pathname)
   );
 }
 
 /**
  * 현재 URL이 캐릭터챗의 URL인지 반환합니다.
+ * @param [url=location] 대상 URL. 지정되지 않았을 경우, 현재 웹 페이지의 URL을 기준으로 합니다.
  * @returns 채팅 URL 일치 여부
  */
-function isCharacterPath(): boolean {
-  return /\/characters\/[a-f0-9]+\/chats\/[a-f0-9]+/.test(location.pathname);
+function isCharacterPath(url: URL | Location = location): boolean {
+  return /\/characters\/[a-f0-9]+\/chats\/[a-f0-9]+/.test(url.pathname);
 }
 
 /**
  * 현재 URL이 스토리챗 빌더의 URL인지 반환합니다.
+ * @param [url=location] 대상 URL. 지정되지 않았을 경우, 현재 웹 페이지의 URL을 기준으로 합니다.
  * @returns 채팅 URL 일치 여부
  */
-function isStoryBuilderPath() {
-  return /^\/builder\/story(\/.*)?$/.test(location.pathname);
+function isStoryBuilderPath(url: URL | Location = location) {
+  return /^\/builder\/story(\/.*)?$/.test(url.pathname);
 }
 
 /**
  * 현재 URL이 ARPG 채팅의 일부인지 반환합니다.
+ * @param [url=location] 대상 URL. 지정되지 않았을 경우, 현재 웹 페이지의 URL을 기준으로 합니다.
  * @returns 채팅 URL 일치 여부
  */
-function isARPGPath(): boolean {
-  return /\/arpg\/[a-f0-9]+\/[a-f0-9]+\/play/.test(location.pathname);
+function isARPGPath(url: URL | Location = location): boolean {
+  return /\/arpg\/[a-f0-9]+\/[a-f0-9]+\/play/.test(url.pathname);
 }
 
 /**
  * 현재 URL이 ARPG 채팅 빌더의 일부인지 반환합니다.
+ * @param [url=location] 대상 URL. 지정되지 않았을 경우, 현재 웹 페이지의 URL을 기준으로 합니다.
  * @returns 채팅 URL 일치 여부
  */
-function isARPGBuilderPath(): boolean {
-  return /\/arpg\/[a-f0-9]+\/builder/.test(location.pathname);
+function isARPGBuilderPath(url: URL | Location = location): boolean {
+  return /\/arpg\/[a-f0-9]+\/builder/.test(url.pathname);
 }
 
 /**
  * 현재 URL이 크래커 결제 페이지인지 확인합니다.
  * @param type 크래커 페이지 내 탭 타입, 혹은 undefined. undefined는 와일드 카드로 처리됩니다.
+ * @param [url=location] 대상 URL. 지정되지 않았을 경우, 현재 웹 페이지의 URL을 기준으로 합니다.
  * @returns 페이지 및 옵션 일치 여부
  */
-function isCrackerPath(type: Undeclarable<CrackerTabType> = undefined): boolean {
-  if (location.pathname != "/cracker") return false;
+function isCrackerPath(type: Undeclarable<CrackerTabType> = undefined, url: URL | Location = location): boolean {
+  if (url.pathname != "/cracker") return false;
   if (type != undefined) {
     switch (type) {
       case CrackerTabType.PURCHASE: {
-        return new URLSearchParams(location.search).get("tab") == "purchase";
+        return new URLSearchParams(url.search).get("tab") == "purchase";
       }
       case CrackerTabType.ATTEND: {
-        return new URLSearchParams(location.search).get("tab") == "free";
+        return new URLSearchParams(url.search).get("tab") == "free";
       }
       case CrackerTabType.AUTO_PURCHASE: {
-        return new URLSearchParams(location.search).get("tab") == "auto-purchase";
+        return new URLSearchParams(url.search).get("tab") == "auto-purchase";
       }
     }
   }
@@ -86,6 +93,7 @@ function isCrackerPath(type: Undeclarable<CrackerTabType> = undefined): boolean 
 
 /**
  * 현재 URL이 크랙 채팅 URL인지 반환합니다.
+ * @param [url=location] 대상 URL. 지정되지 않았을 경우, 현재 웹 페이지의 URL을 기준으로 합니다.
  * @returns 채팅 URL 일치 여부
  */
 function isChattingPath(): boolean {
@@ -94,11 +102,12 @@ function isChattingPath(): boolean {
 
 /**
  * 현재 스토리 / 캐릭터 ID를 반환합니다.
+ * @param [url=location] 대상 URL. 지정되지 않았을 경우, 현재 웹 페이지의 URL을 기준으로 합니다.
  * @returns 현재 캐릭터 / 스토리 ID
  */
-function character(): Nullable<string> {
+function character(url: URL | Location = location): Nullable<string> {
   if (isChattingPath()) {
-    const split = window.location.pathname.substring(1).split("/");
+    const split = url.pathname.substring(1).split("/");
     const characterId = split[1];
     return characterId;
   }
@@ -107,11 +116,12 @@ function character(): Nullable<string> {
 
 /**
  * 현재 채팅방 ID를 반환합니다.
+ * @param [url=location] 대상 URL. 지정되지 않았을 경우, 현재 웹 페이지의 URL을 기준으로 합니다.
  * @returns 현재 채팅방 ID
  */
-function chatRoom(): Nullable<string> {
+function chatRoom(url: URL | Location = location): Nullable<string> {
   if (isChattingPath()) {
-    const split = window.location.pathname.substring(1).split("/");
+    const split = url.pathname.substring(1).split("/");
     const chatRoomId = split[3];
     return chatRoomId;
   }
