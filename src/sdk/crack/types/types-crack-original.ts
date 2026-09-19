0df48a6bf8fd5656ca52b30d8da10e66b34efbe0
skywolf46 <skywolf461@gmail.com>
2026-09-19 19:36:04 +0900

    feat(C2/CRCK): 팬픽 데이터 필드 추가

--- src/sdk/crack/types/types-crack-original.ts
+++ src/sdk/crack/types/types-crack-original.ts
@@ -1,10 +1,17 @@
+import { Undeclarable } from "../../../utils/generic-types";
+
 export class CrackOriginalState {
   constructor(
+    /** 오리지널 여부 */
     public readonly isOriginal: boolean,
+    /** 플랫폼 측의 조치로 인한 편집 불가능 여부 */
     public readonly isEditBlocked: boolean,
+    /** 팬픽 활성화 여부 */
+    public readonly isFanficEnabled: boolean,
   ) {}
 
-  static from(data: any): CrackOriginalState {
-    return new CrackOriginalState(data["isOriginal"], data["isEditBlocked"]);
+  static from(data: Undeclarable<any>): CrackOriginalState {
+    if (!data) return new CrackOriginalState(false, false, false);
+    return new CrackOriginalState(data["isOriginal"] ?? false, data["isEditBlocked"] ?? false, data["isFanficEnabled"] ?? false);
   }
 }
