4d97a560f08a756a076e4f4f778f74a2e3f0858d
skywolf46 <skywolf461@gmail.com>
2026-09-19 18:52:25 +0900

    feat(C2/CRCK/SANI): 설정 추가 및 자동 구매 배너 삭제 기능 추가

--- src/scripts/crack/sanitizer.ts
+++ src/scripts/crack/sanitizer.ts
@@ -1,17 +1,31 @@
 import { CRACK_VERSION_RULE } from "../../constants/script-constants";
 import { CrackSdk } from "../../sdk/crack-sdk";
+import { Undeclarable } from "../../utils/generic-types";
 import { BrowserInitUtil } from "../../utils/init-util";
 import { readonlyLazy } from "../../utils/lazy-util";
+import { LocaleStorageConfig } from "../../utils/local-storage-config";
 import { LogUtil } from "../../utils/log-utils";
 import { NodeLocator } from "../../utils/node-locator-util";
 import { ObserveUtil } from "../../utils/observe-util";
 import { ScriptMetaUtil } from "../../utils/script-meta-util";
+import SCRIPT_STYLE from "./css/sanitizer.scss?inline";
 
 export const scriptMeta = ScriptMetaUtil.construct("crack", "sanitizer.user.js", undefined, (meta) => {
   meta.name = "Chasm Crystallized Sanitizer (결정화 캐즘 손소독제)";
-  meta.version = "CRCK-SANI-v2.0.0" satisfies CRACK_VERSION_RULE;
+  meta.version = "CRCK-SANI-v2.1.0p" satisfies CRACK_VERSION_RULE;
   meta.author = "milkyway0308";
-  meta.description = "모바일 앱 권유 팝업 제거. 손도 깔끔!";
+  meta.description = "필요 없는 광고 배너 제거. 손도 아주 깔끔!";
+});
+
+// =====================================================
+//                      설정
+// =====================================================
+const settings = new LocaleStorageConfig<{
+  removeAppBanner: Undeclarable<boolean>;
+  removeAutoPurchaseBanner: Undeclarable<boolean>;
+}>("chasm-crck-san-settings", {
+  removeAppBanner: true,
+  removeAutoPurchaseBanner: true,
 });
 
 // =====================================================
@@ -19,13 +33,14 @@ export const scriptMeta = ScriptMetaUtil.construct("crack", "sanitizer.user.js",
 // =====================================================
 
 const logger = readonlyLazy(() => new LogUtil("Sanitizer", false));
+const SANITIZER_REMOVAL_KEY = "chasm-sani-disabled";
 
 // =====================================================
 //                        로직
 // =====================================================
 
 function monitor() {
-  if (CrackSdk.environment().isMobile()) {
+  if (settings.config.removeAppBanner && CrackSdk.environment().isMobile()) {
     let scannerIndex = 0;
     for (let element of NodeLocator.getAll<HTMLDivElement>('div[height="64"]')) {
       if (++scannerIndex >= 5) return;
@@ -40,12 +55,38 @@ function monitor() {
   }
 }
 
-// =====================================================
-//                       초기화
-// =====================================================
+function hrefMonitor() {
+  if (settings.config.removeAutoPurchaseBanner && CrackSdk.path().isCrackerPath()) {
+    const selected = NodeLocator.getAll<HTMLImageElement>('img[src="https://cdn-image.static.wrtn.ai/crack/cracker-page-ticket.svg"]');
+    if (selected.length > 0 && selected[0].parentElement?.hasAttribute(SANITIZER_REMOVAL_KEY) == false) {
+      selected[0].parentElement?.setAttribute(SANITIZER_REMOVAL_KEY, "true");
+          logger.log("자동 구매 권유 배너 1개를 제거하였습니다.");
+    }
+  }
+}
+
+// =================================================
+//                  초기화
+// =================================================
+
+function addMenu() {
+  CrackSdk.addonModal()
+    .acquire()
+    .addLicenseDisplay((panel) => {
+      panel.addTitleText("결정화 캐즘 손소독제");
+      panel.addText("- decentralized-modal.js 프레임워크 사용 (https://github.com/milkyway0308/crystalized-chasm/decentralized.js)");
+    });
+}
 
 BrowserInitUtil.init(() => {
+  settings.load();
+  addMenu();
+  BrowserInitUtil.callGMAddStyle(SCRIPT_STYLE);
+  hrefMonitor();
   BrowserInitUtil.onPagePrepare(() => {
     ObserveUtil.attachObserver(document.body, monitor);
+    ObserveUtil.attachHrefObserver(document.body, hrefMonitor);
+    monitor();
+    hrefMonitor();
   });
 });
